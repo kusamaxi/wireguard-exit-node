e75b5ce6e905838b9bf8ecfac32af0f89f7f18cb
hitchhooker <tommi@niemi.lol>
2023-03-12 07:59:50 +0200

    forget cli structure

--- innernet-server-setup.sh
+++ innernet-server-setup.sh
@@ -1,90 +1,86 @@
 #!/bin/bash
 
+# Innernet network settings
 network_name="rotkonet"
 network_cidr="10.42.0.0/16"
-
-# subnet
-subnet_name="xi"
-subnet_nodes=(01 02)
-subnet_cidr="10.42.1.0/24"
-
-# homenet
-homenet_name="pc"
-homenet_name=(01 02)
-homenet_cidr="10.42.1.0/24"
-
-external_endpoint=$(ip a | grep "inet " | awk '{print $2}' | cut -d '/' -f1):51820
-auto_external_endpoint=false
+external_endpoint="127.0.0.1"
 listen_port=51820
 
-while [[ $# -gt 0 ]]
-do
-key="$1"
+# Subnet settings
+subnet_name="xi"
+subnet_cidr="10.42.1.0/24"
+subnet_nodes=("xi01" "xi02")
 
-case $key in
-    --network-name)
-    network_name="$2"
-    shift
-    shift
-    ;;
-    --network-cidr)
-    network_cidr="$2"
-    shift
-    shift
-    ;;
-    --external-endpoint)
-    external_endpoint="$2"
-    shift
-    shift
-    ;;
-    --auto-external-endpoint)
-    auto_external_endpoint=true
-    shift
-    ;;
-    --listen-port)
-    listen_port="$2"
-    shift
-    shift
-    ;;
-    -h|--help)
-    echo "Usage: $0 [--network-name NETWORK_NAME] [--network-cidr NETWORK_CIDR] [--external-endpoint EXTERNAL_ENDPOINT] [--auto-external-endpoint] [--listen-port LISTEN_PORT]"
-    exit 0
-    ;;
-    *)
-    echo "Unknown option: $1"
-    echo "Usage: $0 [--network-name NETWORK_NAME] [--network-cidr NETWORK_CIDR] [--external-endpoint EXTERNAL_ENDPOINT] [--auto-external-endpoint] [--listen-port LISTEN_PORT]"
+# Homenet settings
+homenet_name="pc"
+homenet_cidr="10.42.2.0/24"
+homenet_nodes=("pc01" "pc02")
+
+# Function to create a new innernet network
+function create_network {
+  echo "Creating new innernet network:"
+  echo "    Network name: $network_name"
+  echo "    Network CIDR: $network_cidr"
+  echo "    External endpoint: $external_endpoint"
+  echo "    Listen port: $listen_port"
+  echo ""
+  echo "Press enter to continue, or ctrl-c to cancel"
+  read _
+
+  # create private network
+  innernet-server new --network-name "$network_name" --network-cidr "$network_cidr" --external-endpoint "$external_endpoint" --listen-port "$listen_port"
+}
+
+# Function to create subnet peers
+function create_subnet_peers {
+  # create subnet
+  innernet-server add-cidr --name "$subnet_name" --cidr "$subnet_cidr" --yes "$network_name"
+
+  # loop through subnet nodes
+  for node in "${subnet_nodes[@]}"; do
+    peer_name="$subnet_name-$node"
+
+    # create peer and save config
+    innernet-server add-peer "$network_name" --auto-ip --name "$node" --save-config "$peer_name.toml" --yes
+  done
+}
+
+# Function to create homenet peers
+function create_homenet_peers {
+  # create homenet
+  innernet-server add-cidr --name "$homenet_name" --cidr "$homenet_cidr" --yes "$network_name"
+
+  # loop through homenet nodes
+  for node in "${homenet_nodes[@]}"; do
+    peer_name="$homenet_name-$node"
+
+    # create peer and save config
+    innernet-server add-peer "$network_name" --auto-ip --name "$node" --save-config "$peer_name.toml" --yes
+  done
+}
+
+# Main script
+
+# Check if innernet-server is installed
+if ! command -v innernet-server &> /dev/null
+then
+    echo "innernet-server is not installed. Please install it first."
     exit 1
-    ;;
-esac
-done
-
-if [ "$auto_external_endpoint" = true ]; then
-    external_endpoint=$(ip a | grep "inet " | awk '{print $2}' | cut -d '/' -f1):51820
 fi
 
-echo "Usage: $0 [--network-name NETWORK_NAME] [--network-cidr NETWORK_CIDR] [--external-endpoint EXTERNAL_ENDPOINT] [--auto-external-endpoint] [--listen-port LISTEN_PORT]"
-echo "Creating new innernet network:"
-echo "    Network name: $network_name"
-echo "    Network CIDR: $network_cidr"
-echo "    External endpoint: $external_endpoint"
-echo "    Listen port: $listen_port"
-echo ""
-echo "Press enter to continue, or ctrl-c to cancel"
-read _
+# Check if user is running the script as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
 
-# create private network
-innernet-server new --network-name "$network_name" --network-cidr "$network_cidr" --external-endpoint "$external_endpoint" --listen-port "$listen_port"
+# Create the innernet network
+create_network
 
-# create subnet
-innernet-server add-cidr --name "$subnet_name" --cidr "$subnet_cidr" --yes "$network_name"
-# loop through subnet
-for node in "${subnet_nodes[@]}"; do
-  innernet-server add-peer "$network_name" --auto-ip --name "$node"
-done
+# Create subnet peers
+create_subnet_peers
 
-# create homenet
-innernet-server add-cidr --name "$homenet_name" --cidr "$homenet_cidr" --yes "$network_name"
-# loop through homenet
-for node in "${subnet_nodes[@]}"; do
-  innernet-server add-peer "$network_name" --auto-ip --name "$node"
-done
+# Create homenet peers
+create_homenet_peers
+
+echo "Done!"
