cbc24f3e11d58967ccd4eacb8a2d15e83f1665a1
hitchhooker <tommi@niemi.lol>
2023-03-16 17:22:58 +0000

    new try

--- deprecated/generate-wireguard-config-on-gateway.sh
+++ deprecated/generate-wireguard-config-on-gateway.sh
@@ -6,7 +6,7 @@
 function generate_peer_config {
   local node_name=$1
   local peer_public_key=$2
-  local peer_ip_address="10.0.0.$(( $(echo $node_name | sed 's/[^0-9]*//g') + 1))"
+  local peer_ip_address=$3
   cat << EOF
 # Peer: $node_name
 [Peer]
@@ -29,22 +29,26 @@ EOF
 
 # Function to generate the WireGuard configuration file
 function generate_wireguard_config {
-  local exit_hostname=$(hostname)
   local exit_private_key=$(wg genkey)
   local exit_public_key=$(echo "$exit_private_key" | wg pubkey)
-  cat << EOF > /etc/wireguard/wg0.conf
+  cat << EOF > /etc/wireguard/gw00.conf
 [Interface]
 PrivateKey = $exit_private_key
-Address = 10.0.0.1/24
+Address = 10.42.0.1/16
 ListenPort = 51820
 
 EOF
   for public_key_file in /etc/wireguard/*.public; do
     local node_name=$(basename "${public_key_file%.*}")
-    if [ "$node_name" != "$exit_hostname" ]; then
+    if [ "$node_name" == "xi01" ]; then
       local peer_public_key=$(cat "$public_key_file")
-      cat << EOF >> /etc/wireguard/wg0.conf
-$(generate_peer_config "$node_name" "$peer_public_key")
+      cat << EOF >> /etc/wireguard/gw00.conf
+$(generate_peer_config "$node_name" "$peer_public_key" "10.42.1.1")
+EOF
+    elif [ "$node_name" == "xi02" ]; then
+      local peer_public_key=$(cat "$public_key_file")
+      cat << EOF >> /etc/wireguard/gw00.conf
+$(generate_peer_config "$node_name" "$peer_public_key" "10.42.1.2")
 EOF
     fi
   done
@@ -55,4 +59,4 @@ exit_public_ipv4=$(ip a | awk '/inet / && !/127.0.0.1/ { sub(/\/.*/, "", $2); pr
 exit_public_ipv6=$(ip a | awk '/inet6 / && !/::1/ { sub(/\/.*/, "", $2); print $2; exit }')
 generate_wireguard_config
 
-echo "WireGuard configuration generated at /etc/wireguard/wg0.conf"
+echo "WireGuard configuration generated at /etc/wireguard/gw00.conf"
